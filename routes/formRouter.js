dcbcf67f723a98fe497f40733492675cb45971d4
Dmitrii <dchikashov@gmail.com>
2022-04-07 18:40:07 +0300

    mail

--- routes/formRouter.js
+++ routes/formRouter.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
 const router = require('express').Router();
+const nodemailer = require('nodemailer');
 const { User } = require('../db/models');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 
 router.route('/sendform')
   .post(async (req, res) => {
@@ -10,6 +20,13 @@ router.route('/sendform')
       await User.create({
         name: title, phone, email, img,
       });
+      const mailOptions = {
+        from: 'dchikashovwork@gmail.com',
+        to: email,
+        subject: 'Магазин часов',
+        text: 'Мы приняли ваш заказ',
+      };
+      transporter.sendMail(mailOptions);
       return res.redirect('/');
     }
   });
