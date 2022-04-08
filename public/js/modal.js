faa353a6d271c6008bf90fd7e1788bdf2464cda3
Alex <alexshilikhin@yandex.ru>
2022-04-08 11:25:24 +0300

    NavButAnimForm

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,21 @@
+const navBarUs = document.querySelector('.btn-group');
+const { authorization } = document.forms;
+const closeUs = document.querySelector('#closeAvt');
+
+navBarUs.addEventListener('click', (e) => {
+  if (e.target.innerText === 'Авторизация') {
+    authorization.classList.toggle('formAvt');
+  }
+});
+closeUs.addEventListener('click', (e) => {
+  authorization.classList.toggle('formAvt');
+});
+authorization.addEventListener('submit', (e) => {
+  e.preventDefault();
+  authorization.classList.toggle('formAvt');
+});
+
+
 const navBar = document.querySelector('.btn-group');
 const { registration } = document.forms;
 const closeReg = document.querySelector('#closeReg');
@@ -17,38 +35,20 @@ registration.addEventListener('submit', (e) => {
 });
 
 
-const navBarAvt = document.querySelector('.btn-group');
-const { forma } = document.forms;
-const closeAvt = document.querySelector('#closeUsers');
 
-navBarAvt.addEventListener('click', (e) => {
+const navBarForm = document.querySelector('.btn-group');
+const { formaUser } = document.forms;
+const closeForm = document.querySelector('#closeUsers');
+
+navBarForm.addEventListener('click', (e) => {
   if (e.target.innerText === 'Форма') {
-    forma.classList.toggle('formUsers');
+    formaUser.classList.toggle('formUsers');
   }
 });
-closeAvt.addEventListener('click', (e) => {
-  
-  forma.classList.toggle('formUsers');
+closeForm.addEventListener('click', (e) => {
+  formaUser.classList.toggle('formUsers');
 });
-forma.addEventListener('submit', (e) => {
+formaUser.addEventListener('submit', (e) => {
   e.preventDefault();
-  forma.classList.toggle('formUsers');
-});
-
-
-const navBarUs = document.querySelector('.btn-group');
-const { authorization } = document.forms;
-const closeUs = document.querySelector('#closeAvt');
-
-navBarUs.addEventListener('click', (e) => {
-  if (e.target.innerText === 'Авторизация') {
-    authorization.classList.toggle('formUsers');
-  }
-});
-closeUs.addEventListener('click', (e) => {
-  authorization.classList.toggle('formUsers');
-});
-authorization.addEventListener('submit', (e) => {
-  e.preventDefault();
-  authorization.classList.toggle('formUsers');
+  formaUser.classList.toggle('formUsers');
 });
